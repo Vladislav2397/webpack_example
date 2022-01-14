0661793d0f7730ab3a5fc473c74c7511d0111871
Vladislav <vt@burno.tech>
2022-01-14 19:06:03 +0300

    Backup to default settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,67 @@
-const path = require('path')
-const webpack = require('webpack')
+const VueLoaderPlugin = require('vue-loader-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
-const { VueLoaderPlugin } = require('vue-loader')
-const TerserPlugin = require('terser-webpack-plugin')
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const isProd = process.env.NODE_ENV === 'production'
+const path = require("path");
 
 module.exports = {
-    mode: isProd ? 'production' : 'development',
-    entry: {
-        app: './src/main.js'
-        // critical: './src/assets/scss/critical.scss',
-        // main: './src/assets/scss/main.scss',
-    },
-    devtool: false,
+    mode: 'development',
+    entry: './src/main.js',
     output: {
-        path: path.resolve(__dirname, '../dist'),
-        publicPath: '/dist/',
-        filename: '[name].[chunkhash].js',
+        path: __dirname + '/dist'
     },
     resolve: {
-        extensions: ['*', '.js', '.jsx', '.vue', '.ts', '.tsx'],
+        extensions: [
+            '*', '.js', '.ts', '.vue', '.scss', '.css', '.html'
+        ],
         alias: {
-            '@': path.resolve(__dirname, 'src'),
-            'vue': path.resolve('./node_modules/vue'),
-            'vuex': path.resolve('./node_modules/vuex'),
-            'vue-class-component': path.resolve('./node_modules/vue-class-component'),
-            'vue-property-decorator': path.resolve('./node_modules/vue-property-decorator'),
-        },
+            '@': path.resolve(__dirname, 'src')
+        }
     },
     module: {
-        noParse: /es6-promise\.js$/,
         rules: [
-            {
-                test: /\.svg$/,
-                loader: 'vue-svg-loader',
-                options: {
-                    svgo: {
-                        plugins: [
-                            { collapseGroups: false },
-                            { removeEmptyContainers: false },
-                            { cleanupIDs: false },
-                            { removeViewBox: false },
-                        ],
-                    },
-                },
-            },
-            {
-                test: /\.css$/,
-                use: ['vue-style-loader', 'css-loader'],
-            },
             {
                 test: /\.vue$/,
                 loader: 'vue-loader',
-                options: {
-                    loaders: {
-                        ts: 'ts-loader',
-                    },
-                    esModule: true,
-                    compilerOptions: {
-                        preserveWhitespace: false,
-                    },
-                },
             },
             {
-                test: /\.(jsx?|tsx?)$/,
-                exclude: /node_modules/,
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /main\.scss/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ]
+            },
+            {
+                test: /critical\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ]
+            },
+            {
+                test: /(?<!critical|main)\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'debug-loader',
+                    'sass-loader',
+                ]
+            },
+            {
+                test: /\.pug$/,
+                use: [
+                    'pug-bem-plain-loader',
+                ]
+            },
+            {
+                test: /\.ts$/,
                 use: [
                     'babel-loader',
                     {
@@ -77,87 +72,21 @@ module.exports = {
                             appendTsxSuffixTo: [/\.vue$/],
                         },
                     },
-                ],
-            },
-            {
-                test: /\.(png|jpg|gif)$/,
-                loader: 'url-loader',
-                options: {
-                    limit: 10000,
-                    name: '[name].[ext]?[hash]',
-                },
-            },
-            {
-                test: /\.scss$/,
-                use: isProd
-                    ? [
-                          MiniCssExtractPlugin.loader,
-                          'css-loader',
-                          {
-                              loader: 'postcss-loader',
-                              options: {
-                                  sourceMap: true,
-                                  config: {
-                                      path: `./postcss.config.js`,
-                                  },
-                              },
-                          },
-                          'sass-loader',
-                      ]
-                    : [
-                        'vue-style-loader',
-                        'css-loader',
-                        'sass-loader'
-                    ],
-            },
-            {
-                test: /\.pug$/,
-                use: [
-                    'pug-bem-plain-loader',
-                ],
-            },
-            {
-                test: /\.(frag|vert|glsl)$/,
-                use: [
-                    {
-                        loader: 'glsl-shader-loader',
-                        options: {},
-                    },
-                ],
-            },
-        ],
+                ]
+            }
+        ]
     },
-    performance: {
-        maxEntrypointSize: 300000,
-        hints: isProd ? 'warning' : false,
+    devServer: {
+        static: path.resolve(__dirname, 'public'),
+        port: 1024,
     },
-    optimization: {
-        minimizer: [
-            new TerserPlugin({
-                parallel: true,
-                terserOptions: {
-                    compress: false,
-                    ecma: 6,
-                    mangle: true,
-                },
-            }),
-            new OptimizeCSSAssetsPlugin({}),
-        ],
-        splitChunks: {
-            // include all types of chunks
-            chunks: 'all',
-        },
-    },
-    plugins: isProd
-        ? [
-              new VueLoaderPlugin(),
-              new webpack.optimize.ModuleConcatenationPlugin(),
-              new MiniCssExtractPlugin({
-                  filename: '[name].min.[chunkhash].css',
-              }),
-          ]
-        : [
-              new VueLoaderPlugin(),
-              new FriendlyErrorsPlugin(),
-          ],
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].min.css'
+        }),
+        new VueLoaderPlugin(),
+    ]
 }
